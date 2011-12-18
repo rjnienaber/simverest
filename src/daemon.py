560508717d1ecf9800f82a855d6369d6b9b59d05
Richard Nienaber <rjnienaber@gmail.com>
2011-12-18 00:27:35 +0000

    Made daemon script respond to Ctrl-C

--- src/daemon.py
+++ src/daemon.py
@@ -6,10 +6,13 @@ from health import VarnishHealth
 from stats import VarnishStats
 
 def process_data(host, username, password, varnish):
-    with closing(SSHClient()) as ssh:
-        ssh.set_missing_host_key_policy(AutoAddPolicy())
-        ssh.connect(host, username=username, password=password)
-        varnish.process(ssh)
+    try:
+        with closing(SSHClient()) as ssh:
+            ssh.set_missing_host_key_policy(AutoAddPolicy())
+            ssh.connect(host, username=username, password=password)
+            varnish.process(ssh)
+    except KeyboardInterrupt:
+        pass
     
 def start_process(target, args):
     process = Process(target=target, args=args)
@@ -18,18 +21,24 @@ def start_process(target, args):
     
 if __name__ == "__main__":
     details = tuple(sys.argv[1:4])
-    
-    arguments = [details + (VarnishHealth(),), details + (VarnishStats(),)]
+
+    arguments = [details + (VarnishStats(),), details + (VarnishHealth(),)]
     processes = [{'target': process_data, 'args': a, 'restarts': 0,
                   'process': start_process(target=process_data, args=a)} 
                  for a in arguments]
+    print 'Started gathering varnish data'
     
-    while True:
-        time.sleep(1)
-        deadprocesses = [p for p in processes if not p['process'].is_alive()]
-        for deadprocess in deadprocesses:
-            target, process, args = deadprocess['target'], deadprocess['process'], deadprocess['args']
-            process.terminate()
-            deadprocess['process'] = start_process(target=target, args=args)
-            deadprocess['restarts'] += 1
-            print 'Restarts: %s' % deadprocess['restarts']
+    try:
+        while True:
+            time.sleep(1)
+            deadprocesses = [p for p in processes if not p['process'].is_alive()]
+            for deadprocess in deadprocesses:
+                target, process, args = deadprocess['target'], deadprocess['process'], deadprocess['args']
+                process.terminate()
+                deadprocess['process'] = start_process(target=target, args=args)
+                deadprocess['restarts'] += 1
+                print 'Restarts: %s' % deadprocess['restarts']
+    except KeyboardInterrupt:
+        pass
+        
+    print 'Ending gathering varnish data'
