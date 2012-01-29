9dbeeab08cfdcea4f7cf76c05eaa53db08c83200
Richard Nienaber <rjnienaber@gmail.com>
2012-01-29 13:12:33 +0000

    Hacking around with jsonp support

--- src/web/api.py
+++ src/web/api.py
@@ -1,4 +1,4 @@
-from bottle import Bottle, run, view, redirect, static_file
+from bottle import Bottle, run, view, redirect, static_file, request, response
 import bottle
 import json
 import utils
@@ -19,20 +19,28 @@ def list_servers():
 
 @app.route('/api/server/:name/backends')
 def server_health(name):
-    return read_json(utils.HEALTH_JSON_FILE)
+    return process_jsonp(utils.HEALTH_JSON_FILE)
 
 @app.route('/api/server/:name/stats')
 def server_stats(name):
-    return read_json(utils.STATS_JSON_FILE)
+    return process_jsonp(utils.STATS_JSON_FILE)
 
-def read_json(filePath, mime_type='application/json'):
-    return static_file(filePath, '', mime_type)
+def process_jsonp(file_path):
+    response.content_type = 'application/json'
+    return '{0}({1})'.format(request.query.callback, read_json(utils.HEALTH_JSON_FILE))
+    
+def read_json(file_path):
+    while True:
+        data = utils.read_all(file_path)
+        if data != '':
+            return data
+        time.sleep(0.5)
     
 @app.route('/')
 @view('default')
 def basic_view():
-    view_data = json.loads(utils.read_all(utils.HEALTH_JSON_FILE))
-    view_data.update(json.loads(utils.read_all(utils.STATS_JSON_FILE)))
+    view_data = json.loads(read_json(utils.HEALTH_JSON_FILE))
+    view_data.update(json.loads(read_json(utils.STATS_JSON_FILE)))
     return view_data
 
 def start(varnish_hosts, host='0.0.0.0', port=8080, debug=False):
