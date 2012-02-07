c7b97c0fbb0262744c43f5bc82dc09b0ecadaf58
Richard Nienaber <rjnienaber@gmail.com>
2012-02-07 22:32:28 +0000

    Added testing for stats values

--- src/testing.py
+++ src/testing.py
@@ -6,9 +6,10 @@ def update_files():
     try:
         health_data = json.loads(utils.read_json(utils.HEALTH_JSON_FILE))
         health_data['name'] = 'testing'
-        stats_file = json.loads(utils.read_json(utils.STATS_JSON_FILE))
+        stats_data = json.loads(utils.read_json(utils.STATS_JSON_FILE))
         print 'Generating test data'
         while True:
+            #randomly make a server change status every 10 seconds
             if random.randint(1, 10) == 3:
                 health_data['last_update'] = datetime.now()
                 index = random.randint(0, 4)
@@ -19,7 +20,12 @@ def update_files():
                     
                 utils.dump_data(health_data, utils.HEALTH_JSON_FILE)
         
-            #utils.dump_data(varnish_stats, utils.STATS_JSON_FILE)
+            #update stats
+            stats_data['process']['cpu'] = random.randint(0, 10)
+            for counter in stats_data['varnish']:
+                counter['value'] = random.random() * 100
+            
+            utils.dump_data(stats_data, utils.STATS_JSON_FILE)
             
             time.sleep(1) 
     except KeyboardInterrupt:
