54b7c7fee898a89fb41e04e26f59724c3ef23f61
Richard Nienaber <rjnienaber@gmail.com>
2012-03-11 09:01:23 +0000

    Changed dummy data generator so that it generates fake data for more than one server

--- src/tests/data_generator.py
+++ src/tests/data_generator.py
@@ -7,7 +7,7 @@ from threading import Thread
 from collectors import stats
 
 FAKE_BACKEND_NAMES = ['FAKE-BACKEND1', 'FAKE-BACKEND2', 'FAKE-BACKEND3']
-FAKE_SERVER_NAME = 'TEST-SERVER'
+FAKE_SERVER_NAMES = ['TEST-SERVER1', 'TEST-SERVER2', 'TEST-SERVER3']
 
 class DummyDataGenerator:
     def __init__(self, server_state):
@@ -18,39 +18,39 @@ class DummyDataGenerator:
         server_state = self.server_state
         print('Generating test data')
         
-        for backend_name in FAKE_BACKEND_NAMES:
-            server_state.update_backend(FAKE_SERVER_NAME, backend_name, \
-                                        'healthy')
+        for server in FAKE_SERVER_NAMES:
+            for backend_name in FAKE_BACKEND_NAMES:
+                server_state.update_backend(server, backend_name, 'healthy')
         
         while self.should_continue:
-            #randomly make a server change status every 10 seconds
-            if random.randint(1, 10) == 3:
-                backend_name = random.choice(FAKE_BACKEND_NAMES)
-                state = random.choice(['healthy', 'sick'])
-                server_state.update_backend(FAKE_SERVER_NAME, backend_name,
-                                            state)
-
-            #update stats
-            cpu = random.random() * 25
-            server_state.update_process(FAKE_SERVER_NAME,
-                                        '1400m', '234m', cpu, 4.5)
-
-            counter = 0
-            varnishstats = []
-            for varnish_counter in stats.VARNISH_COUNTERS:
-                value = random.random() * 400
-                counter += 1
-                description = 'Counter ' + str(counter)
-                varnishstats.append({'name': varnish_counter, 
-                                     'value': value, 
-                                     'description': description})
-                              
-            server_state.update_varnishstats(FAKE_SERVER_NAME, varnishstats)
+            for server in FAKE_SERVER_NAMES:
+                self.generate_fake_data(server, server_state)
 
             time.sleep(1)
 
         print('Ending test thread')
+        
+    def generate_fake_data(self, server, server_state):
+        #randomly make a server change status every 10 seconds
+        if random.randint(1, 10) == 3:
+            backend_name = random.choice(FAKE_BACKEND_NAMES)
+            state = random.choice(['healthy', 'sick'])
+            server_state.update_backend(server, backend_name, state)
 
+        #update stats
+        cpu = random.random() * 25
+        server_state.update_process(server, '1400m', '234m', cpu, 4.5)
+
+        counter = 0
+        varnishstats = []
+        for varnish_counter in stats.VARNISH_COUNTERS:
+            value = random.random() * 400
+            counter += 1
+            description = 'Counter ' + str(counter)
+            varnishstats.append({'name': varnish_counter, 'value': value, 
+                                 'description': description})
+                          
+        server_state.update_varnishstats(server, varnishstats)
 
     def stop(self):
         self.should_continue = False    
