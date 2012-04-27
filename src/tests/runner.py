1a2ef29264137e2e51576dd4287fd6e060e206f2
Richard Nienaber <rjnienaber@gmail.com>
2012-04-27 23:17:41 +0100

    Added travis config

--- src/tests/runner.py
+++ src/tests/runner.py
@@ -2,7 +2,7 @@ import os
 import sys
 import discover
 
-os.chdir(os.path.abspath(os.path.dirname(__file__)) + '\..')
+os.chdir(os.path.abspath(os.path.dirname(__file__)) + os.path.altsep + '..')
 
 sys.path += ['../']
 os.system(['clear', 'cls'][os.name == 'nt'])
